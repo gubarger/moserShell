d64aff37c60ec3ebb10019ba5268436df655ef8f
gubarger <kirillkum2001@gmail.com>
2025-09-16 14:24:56 +0300

    Removing unnecessary functions. The first working prototype of my shell (no tests yet)

--- include/executor.h
+++ include/executor.h
@@ -11,16 +11,7 @@
 class Executor {
 public:
     /*---base family---*/
-
-    // nullptr or (char*)nullptr will be specified at the end of all incoming arguments
-    void execl   (const char* path, const char* arg0, const char* arg1, ...) {}
-    void execlp  (const char* file, const char* arg0, const char* arg1, ...) {}
-    void execle  (const char* path, const char* arg0, const char* arg1, ...) {}
-    void execv   (const char* path, char* const argv[]) {}
-    void execvp  (const char* file, char* const argv[]) {}
-    void execvpe (const char* file, char* const argv[], char* const envp[])  {}
-
-    void execute_command(const std::vector<std::string>& tokens); // base test func
+    void execute_command(const std::vector<std::string>& tokens); // base execute func
 };
 
 #endif // INCLUDE_EXECUTOR_H_
